c387f45285e5079e0d79d75f4925cf352eaa5f50
Carlos Galeano <dnny419@gmail.com>
2021-03-14 08:45:49 +0700

    Adding the print_s function

--- functions.c
+++ functions.c
@@ -10,3 +10,26 @@ int print_c(va_list v)
 	_putchar(va_arg(v, int));
 	return (1);
 }
+
+/**
+ * print_s - print string
+ * @v: Value to be printed
+ * Return: void
+ */
+int print_s(va_list v)
+{
+	size_t i;
+	char *str = va_arg(v, char *);
+
+	if (str == NULL)
+	{
+		str = "(null)";
+	}
+
+	for (i = 0; i < str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (i);
+}
